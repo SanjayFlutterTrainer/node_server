9106eaff4c53f8891e414082ad6e3711c27b175f
SanjayFlutterTrainer <fluttertrainerfuturakochi@gmail.com>
2024-06-19 15:38:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// MongoDB Atlas connection
+const mongoURI = 'mongodb+srv://sanjayksanthosh55:nxiW0AEFy8XhbSOj@cluster0.yrimhnb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
+
+// Define a simple schema and model
+const ItemSchema = new mongoose.Schema({
+  name: String,
+  quantity: Number
+});
+
+const Item = mongoose.model('Item', ItemSchema);
+
+// CRUD operations
+// Create
+app.post('/items', async (req, res) => {
+  const newItem = new Item(req.body);
+  try {
+    const item = await newItem.save();
+    res.status(201).send(item);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+// Read
+app.get('/items', async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.status(200).send(items);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+// Update
+app.put('/items/:id', async (req, res) => {
+  try {
+    const item = await Item.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!item) {
+      return res.status(404).send();
+    }
+    res.status(200).send(item);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+// Delete
+app.delete('/items/:id', async (req, res) => {
+  try {
+    const item = await Item.findByIdAndDelete(req.params.id);
+    if (!item) {
+      return res.status(404).send();
+    }
+    res.status(200).send(item);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
